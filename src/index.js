66893a26444d987d3ae951c9e5256e7b2a8af0c9
Maspanova <105561456+Maspanova@users.noreply.github.com>
2023-02-10 15:02:01 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@
 module.exports = function towelSort (matrix) {
   let arr = [];
   if (matrix==undefined){return []};
-  for (let r=0; r<matrix.lenght; r ++) {
-    for (let c=0; c<matrix[r].lenght; c++) {
+  for (let r=0; r<matrix.length; r ++) {
+    for (let c=0; c<matrix[r].length; c++) {
+      if (r%2==0){
       arr.push(matrix[r][c]);
     }
-  
-  
+    else {
+      arr.push(matrix[r][matrix[r].length-c-1]);
+    }
+}
   }
   return arr;
 }
